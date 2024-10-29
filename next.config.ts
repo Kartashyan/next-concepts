5a96cc5aca018bcc9b1ccabe21d378281035b1d8
Elises <yeghishe.kartashyan@gmail.com>
2024-10-29 21:17:42 +0100

    feat: add environment variables and image domains to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    api_url: process.env.API_URL,
+    access_key: process.env.ACCESS_KEY,
+  },
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
