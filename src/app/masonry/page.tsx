45baee0b10277e2490fcde9c75d33214b2d7d00a
Elises <yeghishe.kartashyan@gmail.com>
2024-10-30 23:23:01 +0100

    refactor: update MasonryPage component to use initial page and query parameters in getPhotos function

--- src/app/masonry/page.tsx
+++ src/app/masonry/page.tsx
@@ -1,11 +1,14 @@
 import { getPhotos } from "./get-photos";
 import { PhotoGrid } from "./photo-grid";
 
+const INITIAL_PAGE = 1;
+const INITIAL_QUERY = 'cat';
+
 export const MasonryPage = async () => {
-  const { data } = await getPhotos();
+  const { data } = await getPhotos({page: INITIAL_PAGE, query: INITIAL_QUERY});
   return (
     <div className="">
-      <PhotoGrid data={data.results} />
+      <PhotoGrid initialData={data.results} />
     </div>
   );
 };
