13fd4e9e218496b8b6d9c7f3be8d735583b11614
Elises <yeghishe.kartashyan@gmail.com>
2024-10-30 23:23:42 +0100

    refactor: Update PhotoGrid component to use initial data and implement infinite scroll
    - Refactor the PhotoGrid component to accept initial data as a prop and implement infinite scroll functionality.
    - Add useState hook to manage the state of photos and page number.
    - Implement the loadMore function to fetch additional photos from the API and update the state accordingly.
    - Update the mapping of photos to use the updated state variable.
    - Add a sentinel element with a ref to trigger the loadMore function when scrolled to the bottom of the grid.
    - Update the alt attribute of the Image component to use the photo's description or alt_description if available.

--- src/app/masonry/photo-grid.tsx
+++ src/app/masonry/photo-grid.tsx
@@ -1,33 +1,46 @@
 "use client";
-import Image from 'next/image';
-import { BasicPhoto } from './unsplash';
-import { useInfiniteScroll } from './use-infinite-scroll';
+import Image from "next/image";
+import { BasicPhoto } from "./unsplash";
+import { useInfiniteScroll } from "./use-infinite-scroll";
+import { useState } from "react";
+import { getPhotos } from "./get-photos";
 
-export const PhotoGrid = ({ data }: {
-  data: BasicPhoto[];
+export const PhotoGrid = ({
+  initialData: initialData,
+}: {
+  initialData: BasicPhoto[];
 }) => {
-  const lastItemRef = useInfiniteScroll(!data, () => console.log("load more"));
+  const [photos, setPhotos] = useState<BasicPhoto[]>(initialData);
+  const [page, setPage] = useState(1);
+
+  const loadMore = async () => {
+    const { data } = await getPhotos({ page: page + 1, query: "cat" });
+    setPhotos((prev) => [...prev, ...data.results]);
+    setPage((prev) => prev + 1);
+  };
+
+  const lastItemRef = useInfiniteScroll(!initialData, loadMore);
+  
   return (
     <div className="relative grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {data.map((photo) => (
+      {photos.map((photo) => (
         <Photo key={photo.id} photo={photo} />
       ))}
-      <div role="sentinel" ref={lastItemRef} className='w-full h-[1px]'/>
+      <div role="sentinel" ref={lastItemRef} className="w-full h-[1px]" />
     </div>
   );
-}
+};
 
 function Photo({ photo }: { photo: BasicPhoto }) {
   return (
     <div className="relative overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1">
       <Image
         src={photo.urls.small}
-        width={photo.width/10}
-        height={photo.height/10}
-        alt={photo.description || photo.alt_description || 'Photo'}
+        width={photo.width / 10}
+        height={photo.height / 10}
+        alt={photo.description || photo.alt_description || "Photo"}
         className="object-cover w-full h-full"
       />
     </div>
   );
 }
-
